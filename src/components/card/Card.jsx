4c0f3cf0c3a24a42b4c3f58cd35e6eadda33d175
gooogler969 <juraevboburjon73@gmail.com>
2025-01-23 11:12:29 +0500

    add cards style

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,26 +6,23 @@ import Cards from "../../data/arrials-data.json";
 function Card() {
   return (
     <div className="card">
-      <div className="card-container">
-        {Cards.map((card) => {
-          return (
-            <div className="card-wrapper">
-              <div className="card-image">
-                <img src={card.img} alt="" />
+      <div className="container card-container">
+        {Cards.map((card) => (
+          <div key={card.id} className="card-wrapper">
+            <div className="card-image">
+              <img src={card.img} alt={card.title} />
+            </div>
+            <div className="card-content">
+              <h3>{card.title}</h3>
+              <div className="raiting">
+                <p>{card.raiting}</p>
               </div>
-              <div className="card-content">
-                <h3>{card.title}</h3>
-                <div className="raiting">
-                  <img src={card.raiting} alt="" />
-                  <p>{card.raiting}</p>
-                </div>
-                <div className="price">
-                  <p>{card.price}</p>
-                </div>
+              <div className="price">
+                <p>{card.price}</p>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
