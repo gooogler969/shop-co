bac3a50a26a21b059c74d53b6272c1e4e6e39de8
gooogler969 <juraevboburjon73@gmail.com>
2025-01-28 06:49:24 +0500

    add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <p>© 2023 - All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navigation/Navbar";
 import Main from "./components/main/Main";
 import Logos from "./components/logos/Logos";
 import Products from "./components/products/Products";
+import Footer from "./components/footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,8 +14,8 @@ root.render(
       <Navbar />
       <Main />
       <Logos />
-
       <Products />
+      <Footer />
     </div>
   </React.StrictMode>
 );
